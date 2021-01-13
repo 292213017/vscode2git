23cde5568a28bd56d7967eac040dd34ea061ea80
292213017 <18859275929@163.com>
2021-01-13 16:21:42 +0800

    配置了一下

--- hello_git.cpp
+++ hello_git.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+    for(int i = 0 ; i<10 ;i++)
+    {
+        if()
+        {
+            int ss1;
+            for(int i = 5)
+            {
+                int assdasdas;
+
+            }
+        }
+    }
     return 0;
 }
